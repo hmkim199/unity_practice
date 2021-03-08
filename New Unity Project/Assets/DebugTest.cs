5aaebb8e3306c6ea988bd24dd9893180cc4c929b
hmkim199 <hmkim199@gmail.com>
2021-03-08 11:22:59 +0900

    unity component, console log practice

---
+++ New Unity Project/Assets/DebugTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DebugTest : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("콘솔 뷰에 데이터 출력하기!!");
+    }
+}
