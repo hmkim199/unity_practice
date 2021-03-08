d7acb752fa25dd7489d6866e3350195430fa76db
hmkim199 <hmkim199@gmail.com>
2021-03-08 14:55:37 +0900

    game object physics N collision practice

---
+++ 2DGameBagic/Assets/Movement2D.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+public class Movement2D : MonoBehaviour
+{
+    private float moveSpeed = 5.0f;                 // 이동 속도
+    /*    private Vector3 moveDirection = Vector3.zero;   // 이동 방향
+    */
+    private Rigidbody2D rigid2D;
+
+    private void Awake()
+    {
+        rigid2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        // Negative left, a : -1
+        // Positive right, d : 1
+        // Non : 0
+        float x = Input.GetAxisRaw("Horizontal"); // 좌우 이동
+
+        // Negative down, s : -1
+        // Positive up, w : 1
+        // Non : 0
+        float y = Input.GetAxisRaw("Vertical");   // 위 아래 이동
+
+        /*// 이동 방향 설정
+        moveDirection = new Vector3(x, y, 0);
+
+        // 새로운 위치 = 현재 위치 + 방향 * 속도
+        transform.position += moveDirection * moveSpeed * Time.deltaTime; // 1초에 1만큼 움직이도록*/
+
+        // Rigidbody 2D 컴포넌트에 있는 속력(velocity) 변수 설정
+        rigid2D.velocity = new Vector3(x, y, 0) * moveSpeed;
+    }
+}
---
+++ 2DGameBagic/Assets/TriggerEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerEvent : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject moveObject;
+    [SerializeField]
+    private Vector3 moveDirection;
+    private float moveSpeed;
+
+    private void Awake()
+    {
+        moveSpeed = 5.0f;
+    }
+
+    // 충돌이 일어나는 순간 1회 호출
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // moveObject 오브젝트의 색상을 검은색(Color.black)으로 설정한다.
+        moveObject.GetComponent<SpriteRenderer>().color = Color.black;
+    }
+
+    // 충돌이 유지되는 동안 매 프레임 호출
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        // moveObject 오브젝트를 moveDirection 방향으로 이동한다.
+        moveObject.transform.position += moveDirection * moveSpeed * Time.deltaTime; 
+    }
+
+    // 충돌이 종료되는 순간 1회 호출
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // moveObject 오브젝트의 색상을 흰색(Color.white)으로 설정한다
+        moveObject.GetComponent<SpriteRenderer>().color = Color.white;
+        // moveObject 오브젝트의 위치를 (0, 4, 0)으로 설정한다.
+        moveObject.transform.position = new Vector3(0, 4, 0);
+    }
+
+}
