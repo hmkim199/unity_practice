19a829598060070b87c0cff91eb47d2060633729
hmkim199 <hmkim199@gmail.com>
2021-03-09 14:08:10 +0900

    characterController practice

---
+++ 3DGameBasic01/Assets/Movement3D.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Movement3D : MonoBehaviour
+{
+    [SerializeField]
+    private float moveSpeed = 5.0f; // 이동 속도
+    [SerializeField]
+    private float jumpForce = 3.0f;
+    private float gravity = -9.81f; // 중력 계수
+    private Vector3 moveDirection;  // 이동 방향
+
+    [SerializeField]
+    private Transform cameraTransform;  // 카메라 Transform 컴포넌트
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    private void Update()
+    {
+        // 중력 설정
+        if (characterController.isGrounded == false) {
+            moveDirection.y += gravity * Time.deltaTime;
+        }
+
+        characterController.Move(moveDirection * moveSpeed * Time.deltaTime);
+    }
+
+    public void MoveTo(Vector3 direction) {
+        // moveDirection = direction;
+        // moveDirection = new Vector3(direction.x, moveDirection.y, direction.z);
+        Vector3 movedis = cameraTransform.rotation * direction;
+        moveDirection = new Vector3(movedis.x, moveDirection.y, movedis.z);
+    }
+
+    public void JumpTo() {
+        if (characterController.isGrounded == true) {
+            moveDirection.y = jumpForce;
+        }
+    }
+}
---
+++ 3DGameBasic01/Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private KeyCode jumpKeyCode = KeyCode.Space;
+    [SerializeField]
+    private CameraController cameraController;
+    private Movement3D movement3D;
+
+    private void Awake()
+    {
+        movement3D = GetComponent<Movement3D>();
+    }
+
+    private void Update()
+    {
+        // x, z방향 이동
+        float x = Input.GetAxisRaw("Horizontal"); // 방향키 좌우 움직임
+        float z = Input.GetAxisRaw("Vertical");   // 방향키 위아래 움직임
+
+        movement3D.MoveTo(new Vector3(x, 0, z));
+
+        // 점프키를 눌러 y축 방향으로 뛰어오르기 (Jump)
+        if (Input.GetKeyDown(jumpKeyCode)) {
+            movement3D.JumpTo();
+        }
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        cameraController.RotateTo(mouseX, mouseY);
+    }
+}
