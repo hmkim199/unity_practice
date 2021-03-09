5ab44e6195eb52c1fe47e2c079239ed809c640d8
hmkim199 <hmkim199@gmail.com>
2021-03-09 10:40:43 +0900

    sprite Atlas, animator, animation practice

---
+++ 2DGameBasic05/Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            animator.SetTrigger("isDie");
+            // animator.SetBool();
+            // animator.SetFloat();
+            // animator.SetInteger();
+        }
+    }
+
+    // 애니메이션 재생 도중 함수 호출. public이어야 애니메이터에서 호출 가능
+    public void OnDieEvent() {
+        Debug.Log("End of Die Animation");
+    }
+
+}
